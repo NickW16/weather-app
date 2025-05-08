762430c6d4ef5412d920349c45978d53af5c73b7
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-05-08 10:49:40 -0300

    make API function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./style.css";
 
 console.log('test');
+
+const weatherData = document.getElementById('weather-data');
+const resultDiv = document.getElementById('result');
+
+async function receiveData(content) {
+    content = weatherData.value;
+    if (content === '') {
+        content = 'riodejaneiro'
+    }
+
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${content}?key=HWSEF7KS97GJ47ABSK7P2AN6B`, {mode: 'cors'})
+    const apiData = await response.json();
+    resultDiv.textContent = apiData.resolvedAddress;
+
+    console.log(resultDiv.textContent);
+}
+
+receiveData().catch(e => {
+    console.log(e);
+});
+
+receiveData();
