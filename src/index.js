1facc9ec9fb5c6e910525a2f12a0f84bfec838b9
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-05-15 11:14:30 -0300

    Adjust error handling

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./style.css";
 console.log('test');
 
 (function (){
-    const weatherData = document.getElementById('weather-data');
+    const inputData = document.getElementById('input-data'); // this is the input div
     const resultDiv = document.getElementById('result');
     resultDiv.classList.add('result-div');
     const submitBtn = document.getElementById('submit-city');
+    const errorMessage = document.getElementById('error');
 
     // this displays the current weather information
     let currentWeatherDiv = document.createElement('div');
@@ -24,18 +25,25 @@ console.log('test');
         currentWeatherDiv.textContent = '';
         firstSection.textContent = '';
         secondSection.textContent = '';
+        errorMessage.textContent = '';
         receiveData();
     });
 
     async function receiveData(content) {
-        content = weatherData.value;
+        content = inputData.value;
         if (content === '') {
             content = 'riodejaneiro'
         }
 
         // this section fetches data from the API
         // it fetches data from the current day up to the 5 next days
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${content}/next5days?unitGroup=metric&include=current&key=HWSEF7KS97GJ47ABSK7P2AN6B`)
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${content}/next6days?unitGroup=metric&include=current&key=HWSEF7KS97GJ47ABSK7P2AN6B`)
+        
+        if (!response.ok) {
+            errorMessage.textContent = 'Error, could not find weather data for given city. Try again';
+            errorMessage.style.display = 'block';
+        }
+
         const apiData = await response.json();
 
         // this section gets the current weather condition and appends all to a div
@@ -83,14 +91,14 @@ console.log('test');
         resultDiv.appendChild(secondSection);
 
         // add all of address' info:
-        function displayWeatherForecast(day) {
-        
+        function displayWeatherForecast(day) {        
+
             const eachDayDiv = document.createElement('div'); // this is just for styling
             eachDayDiv.classList.add('each-day-div');
 
             const currentDate = document.createElement('div');
             //convert 'datetime' into a date object like '2025-01-01'
-            const dateObj = new Date(apiData.days[day].datetime);
+            const dateObj = new Date(apiData.days[day+1].datetime); // "+1" is for the api to not be stupid and prove the yesterday's weather data
             console.log(dateObj);
             // get weekday name (e.g., "Thursday")   
             const weekdayName = dateObj.toLocaleDateString('en-US', { weekday: 'long' });
@@ -115,16 +123,20 @@ console.log('test');
             secondSection.appendChild(eachDayDiv);
             }
 
-        //displays results for the next 5 days
-        for(let i = 0; i <= 5; i++) {
-            displayWeatherForecast(i);
+            //displays results for the next 5 days
+            for(let i = 0; i <= 5; i++) {
+                displayWeatherForecast(i);
+            }
+
+            console.log(resultDiv.textContent);
         }
 
-        console.log(resultDiv.textContent);
-    }
-
-    receiveData().catch(e => { //this handles errors
-        console.log(e);
-    });
+    // receiveData().catch(e => { //this handles errors
+    //     console.log(e);
+    //     const errorMessage = document.getElementById('error');
+    //     errorMessage.textContent = 'error';
+    //     errorMessage.style.display = 'block';
+    // });
 
 })();
+
